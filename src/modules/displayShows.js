0ad9e967749641d09f7d994ec9a861ebc671f18f
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-03-08 17:34:36 -0300

    Add comment button, likes icon, and some styling

--- src/modules/displayShows.js
+++ src/modules/displayShows.js
@@ -6,27 +6,33 @@ export const displayShows = () => {
 
 getShows().then((data) => {
   data.forEach(show => {
-    const title = document.createElement('h3');
-    const textTitle = document.createTextNode(show.name);
-    
-    const image = document.createElement('img');
-    image.setAttribute('src', show.image.medium);
-    
+    // Create card
     const card = document.createElement('div');
     card.classList = 'card';
-    
+    // Create title
+    const title = document.createElement('h3');
+    const textTitle = document.createTextNode(show.name);
+    // Create image
+    const image = document.createElement('img');
+    image.setAttribute('src', show.image.medium);
+    // Create comment button
     const comment = document.createElement('input');
     comment.setAttribute('type', 'button');
     comment.setAttribute('value', 'Comments');
     comment.id = `comments-${show.id}`;
-
-
+    // Create likes icon
+    const likes = document.createElement('i');
+    likes.classList.add('fa-solid', 'fa-heart');
+    // Create a container for title and likes
+    const titleContainer = document.createElement('div');
+    // Append everything
     title.appendChild(textTitle);
+    titleContainer.appendChild(title);
+    titleContainer.appendChild(likes);
     card.appendChild(image);
-    card.appendChild(title);
+    card.appendChild(titleContainer);
     card.appendChild(comment);
     container.appendChild(card);
-    
   });
 });
 }
