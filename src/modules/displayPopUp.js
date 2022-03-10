8ef75fa36e5f9b9b7d8b515d7543ca30ba480640
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-10 16:58:13 +0200

    Adding comments feature

--- src/modules/displayPopUp.js
+++ src/modules/displayPopUp.js
@@ -1,15 +1,31 @@
-import { bgModal, preparePopUp, loadShows } from './preparePopUp.js';
+import {
+  bgModal, preparePopUp, loadShow, displayComments,
+} from './preparePopUp.js';
+import { postComment } from './getComments.js';
 
 export default function showPopUP(e) {
-  loadShows(e.target.id).then((data) => {
-    preparePopUp(data);
-  });
-  bgModal.style.display = 'block';
-  setTimeout(() => {
-    const bgModal = document.querySelector('.bg-modal');
-    const close = document.querySelector('.close');
-    close.addEventListener('click', () => {
-      bgModal.style.display = 'none';
+  loadShow(e.target.id)
+    .then((data) => {
+      preparePopUp(data);
+      bgModal.style.display = 'block';
+      document.querySelectorAll('form').forEach((commentForm) => {
+        commentForm.addEventListener('submit', (event) => {
+          event.preventDefault();
+          const username = commentForm.querySelector('.username').value;
+          const comment = commentForm.querySelector('.commentBody').value;
+          const itemId = commentForm.id;
+          postComment(itemId, username, comment);
+          commentForm.querySelector('.username').value = '';
+          commentForm.querySelector('.commentBody').value = '';
+          displayComments(itemId, false, comment);
+        });
+      });
+    })
+    .then(() => {
+      const bgModal = document.querySelector('.bg-modal');
+      const close = document.querySelector('.close');
+      close.addEventListener('click', () => {
+        bgModal.style.display = 'none';
+      });
     });
-  }, 1000);
 }
--- src/modules/getComments.js
+++ src/modules/getComments.js
@@ -1,9 +1,24 @@
-const commentAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vi4g7NV2Fb946BDtrkSF/comments?item_id=';
+const getCommentsAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vi4g7NV2Fb946BDtrkSF/comments?item_id=';
+const postCommentApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vi4g7NV2Fb946BDtrkSF/comments';
 
 const getComments = async (id) => {
-  const response = await fetch(commentAPI + id);
+  const response = await fetch(getCommentsAPI + id);
   const responseJson = await response.json();
   return responseJson;
 };
 
-export { commentAPI, getComments };
+const postComment = async (id, username, comment) => {
+  await fetch(postCommentApi, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: id,
+      username,
+      comment,
+    }),
+  });
+};
+
+export { getComments, postComment };
